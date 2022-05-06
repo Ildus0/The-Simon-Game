c7b163b4fea3c43a26875e3f2e8d04af1b1791e5
ILDUS <yakupovildus86@gmail.com>
2022-05-06 08:52:20 +0400

    any

--- game.js
+++ game.js
@@ -6,25 +6,28 @@ let arrColorsForCompare = [];
 $('.btn').click(function (y) {
     // arrForCompare.push($(".btn").attr('id'));
     console.log(y.target.id);
-arrColorsForCompare.push(y.target.id);
-console.log(arrColorsForCompare);
+    arrColorsForCompare.push(y.target.id);
+    console.log(arrColorsForCompare);
 });
 
 function genNum() {
     let num = Math.floor(Math.random() * 4);
-   console.log(num);
-    if (num ===  0) {
+    console.log(num);
+    if (num === 0) {
         arrGenColors.push("green");
-    }   if (num ===1) {
-            arrGenColors.push("red");
-        } if (num === 2) {
-                arrGenColors.push("yellow");
-            } if (num===3) {
-                arrGenColors.push("blue");
-            }
-            console.log(arrGenColors);
-            //adn need code to show visual next button
-        }
+    }
+    if (num === 1) {
+        arrGenColors.push("red");
+    }
+    if (num === 2) {
+        arrGenColors.push("yellow");
+    }
+    if (num === 3) {
+        arrGenColors.push("blue");
+    }
+    console.log(arrGenColors);
+    //adn need code to show visual next button
+}
 
 $(document).keypress(function () {
     genNum();
